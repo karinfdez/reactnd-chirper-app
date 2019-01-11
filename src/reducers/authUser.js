595a059d948ae587fe4fcba52b2d2df0bb491206
karinfdez <karinfdez@yahoo.com>
2019-01-10 21:37:11 -0500

    Fix spme errors

--- src/reducers/authUser.js
+++ src/reducers/authUser.js
@@ -1,9 +1,9 @@
-import { SEND_AUTH_USER } from '../actions/authUser';
+import { SET_AUTHED_USER } from '../actions/authUser';
 
 
 export default function authUser(state = null, action) {
     switch(action.type) {
-        case SEND_AUTH_USER: 
+        case SET_AUTHED_USER: 
             return action.id
         default:
             return state;
