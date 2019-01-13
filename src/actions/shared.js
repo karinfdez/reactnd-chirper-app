7cbec36104108cb723dea4f03a6fcbcd12bc352c
karinfdez <karinfdez@yahoo.com>
2019-01-12 19:57:19 -0500

    Add mapStateProps to render Dashboard comp

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,18 +1,16 @@
 import { getInitialData } from '../utils/api';
 import { receiveUsers } from './users';
 import { receiveTweets } from './tweets';
-import { setAuthUser } from './authUser';
+import { setAuthedUser } from './authUser';
 
 const authUser = 'karinfdez';
 export function handleInitialData () {
     return (dispatch) => {  //This func will use the Redux thunk pattern to do an asynchronous request
         return getInitialData()
         .then(({ users, tweets }) => {  //Add these to the redux store
-            console.log('USERS', users);
-            console.log('TWEETS', tweets);
             dispatch(receiveUsers(users))   //Dispatching actions to redux store
             dispatch(receiveTweets(tweets))
-            dispatch(setAuthUser(authUser))
+            dispatch(setAuthedUser(authUser))
         })
     }
 }
