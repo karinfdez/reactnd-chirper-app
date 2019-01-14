7b141c32ff581fa87eb9a003c7a4d4c5ebf58ccd
karinfdez <karinfdez@yahoo.com>
2019-01-14 14:47:54 -0500

    Add logic for liking tweets

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,12 +4,19 @@ import { formatTweet, formatDate } from '../utils/helpers'
 import {TiArrowBackOutline} from 'react-icons/ti'
 import {TiHeartOutline} from 'react-icons/ti'
 import {TiHeartFullOutline} from 'react-icons/ti'
+import { handleToggleTweet } from '../actions/tweets'
 
 class Tweet extends Component {
   handleLike = (e) => {
     e.preventDefault()
 
-    // todo: Handle Like Tweet
+    const { dispatch, tweet, authedUser } = this.props
+
+    dispatch(handleToggleTweet({
+      id: tweet.id,
+      hasLiked: tweet.hasLiked,
+      authedUser
+    }))
   }
   toParent = (e, id) => {
     e.preventDefault()
--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,15 +1,23 @@
-import { RECEIVE_TWEETS } from '../actions/tweets';
+import { RECEIVE_TWEETS, TOGGLE_TWEET } from '../actions/tweets'
 
-
-export default function tweets (state = {}, action) {  //reducer sets default state to an empty obj
-    
-    switch(action.type) {
-        case RECEIVE_TWEETS :
-            return {
-                ...state,
-                ...action.tweets
-            }
-        default :
-            return state;
-    }
+export default function tweets (state = {}, action) {
+  switch(action.type) {
+    case RECEIVE_TWEETS :
+      return {
+        ...state,
+        ...action.tweets
+      }
+    case TOGGLE_TWEET :
+      return {
+        ...state,
+        [action.id]: {
+          ...state[action.id],
+          likes: action.hasLiked === true
+            ? state[action.id].likes.filter((uid) => uid !== action.authedUser)
+            : state[action.id].likes.concat([action.authedUser])
+        }
+      }
+    default :
+      return state
+  }
 }
