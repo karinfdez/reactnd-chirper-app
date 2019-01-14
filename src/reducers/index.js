c5b79a3aee78a7ede1bd2069c875aaff230f4c46
karinfdez <karinfdez@yahoo.com>
2019-01-14 14:48:24 -0500

    Add reducer for loading bar comp

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import authedUser from './authUser';
 import users from './users';
 import tweets from './tweets';
+import { loadingBarReducer } from 'react-redux-loading';
 
 
 export default combineReducers({
     authedUser,
     users,
-    tweets
+    tweets,
+    loadingBar: loadingBarReducer
 })
