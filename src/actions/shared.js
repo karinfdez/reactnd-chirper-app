48111e41c7a4a6000e752799344ab94ec16717ce
karinfdez <karinfdez@yahoo.com>
2019-01-20 11:42:23 -0500

    Solve some conflict

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import { getInitialData } from '../utils/api';
-import { receiveUsers } from './users';
-import { receiveTweets } from './tweets';
-import { setAuthedUser } from './authUser';
-import { showLoading, hideLoading } from 'react-redux-loading';
-
-const authUser = 'karinfdez';
-export function handleInitialData () {
-    return (dispatch) => {  //This func will use the Redux thunk pattern to do an asynchronous request
-        dispatch(showLoading())
-        return getInitialData()
-        .then(({ users, tweets }) => {  //Add these to the redux store
-            dispatch(receiveUsers(users))   //Dispatching actions to redux store
-            dispatch(receiveTweets(tweets))
-            dispatch(setAuthedUser(authUser))
-            dispatch(hideLoading())
-        })
-    }
-=======
 import { getInitialData } from '../utils/api'
 import { receiveUsers } from '../actions/users'
 import { receiveTweets } from '../actions/tweets'
@@ -37,5 +17,4 @@ export function handleInitialData () {
         dispatch(hideLoading())
       })
   }
->>>>>>> c53a69a8b09a0dcab378761f683781c49ead072d
 }
--- src/actions/users.js
+++ src/actions/users.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-export const RECEIVE_USERS = 'RECEIVE_USERS';
-
-export function receiveUsers (users) {
-    return {
-        type: RECEIVE_USERS,
-        users
-    }
-=======
 export const RECEIVE_USERS = 'RECEIVE_USERS'
 
 export function receiveUsers (users) {
@@ -14,5 +5,4 @@ export function receiveUsers (users) {
     type: RECEIVE_USERS,
     users,
   }
->>>>>>> c53a69a8b09a0dcab378761f683781c49ead072d
 }
